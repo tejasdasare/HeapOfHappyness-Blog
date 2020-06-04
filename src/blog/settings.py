9101f8233bde89e4ee8242464a389f183f307da9
tejasdasare <39580185+tejasdasare@users.noreply.github.com>
2020-06-04 15:11:26 +0530

    static nmedia root name change

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -140,11 +140,11 @@ STATICFILES_DIRS = [
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #tinymce
 
